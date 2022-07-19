e146263acdb90be8ae3b6dcf8d2b25fbcf83ef84
1hugol <1hugoamorim@gmail.com>
2022-07-19 18:15:59 -0300

    findByDepartment implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -101,7 +101,13 @@ public class Seller implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", department=" + department + "]";
+		return "--------------------"
+				+"\nSeller id: " + id 
+				+ "\nName: " + name 
+				+ "\nEmail: " + email 
+				+ "\nBirthDate: " + birthDate 
+				+ "\nBaseSalary: " + baseSalary 
+				+ "\nDepartment " + department
+				+ "\n--------------------";
 	}
 }
