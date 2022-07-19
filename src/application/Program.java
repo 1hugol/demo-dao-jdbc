ecf69d8c689d5e70ff4d19ee43eb98f2d5962cda
1hugol <1hugoamorim@gmail.com>
2022-07-19 19:45:50 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,10 @@
 package application;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,35 +12,22 @@ public class Program {
 	public static void main(String[] args) {
 				
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+						
+		List<Seller> list = new ArrayList<>();
+						
+		System.out.println("=== TEST 1: Seller Update ===");
 		
-		System.out.println("=== TEST 1: Seller Find by Id ===");
+		Seller seller = sellerDao.findById(1);
+		seller.setEmail("marthawaine@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("UPDATE COMPLETED!");
 		
-		Seller seller = sellerDao.findById(3);
-		
-		System.out.println(seller);
-		
-		System.out.println("=== TEST 2: Seller Find by Department ===");
-		
-		Department department = new Department(2, null);
-		
-		List<Seller> list = sellerDao.findByDepartment(department);
-		
-		for(Seller obj : list) {
-			System.out.println(obj);
-		}
-		
-		System.out.println("=== TEST 3: Seller Find All ===");
+		System.out.println("=== TEST 2: Seller Find All ===");
 
 		list = sellerDao.findAll();
 
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("=== TEST 4: Seller Insert ===");
-		
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
-		sellerDao.insert(newSeller);
-		System.out.println(newSeller);
 	}
 }
